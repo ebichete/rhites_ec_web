a5805610c486f81c6fa428b63abd9a4e97929985
Assey Alfred M <asseym@gmail.com>
2018-02-06 11:36:52 +0300

    some migration cleanups, requirements file and login default page settings

---
+++ cannula/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cannula', '0011_auto_20180202_1331'),
+        ('cannula', '0010_auto_20180114_0801'),
+    ]
+
+    operations = [
+    ]
--- rhites_ec_web/settings.py
+++ rhites_ec_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w5&z#+w&ur=+e_fhjs#xy7ksc5a*y-&g^4gjc8#5via=c5d%&t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rhites-ec.localhost',]
+ALLOWED_HOSTS = ['rhites-ec.localhost','localhost']
 
 
 # Application definition
@@ -115,3 +115,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 SOURCE_DOC_DIR = os.path.join(BASE_DIR, 'source_doc_storage')
+
+LOGIN_REDIRECT_URL = '/'
